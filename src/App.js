350a0d2b2719b34d1fefbc0b2841d4d3bedf22cf
harikae86 <harmath.esztike@gmail.com>
2022-02-08 11:08:36 +0100

    fix styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import Card from "./components/Card";
 const data = [
   {
-    name: "Gipsz Jakab",
-    restaurant: "Restaurant",
-    address: "sadikghsdfghp",
+    name: "John Freeman",
+    restaurant: "PIZZA THIEF",
+    address: "2610 NW Vaughn st, Portland,",
+    address2: "OR 97210, United States",
   },
   {
-    name: "Gipsz Jakab",
-    restaurant: "Restaurant",
-    address: "sadikghsdfghp",
+    name: "Angela Sliders",
+    restaurant: "MAX STEAKS",
+    address: "3653 Germantown Ave, Philadelpia",
+    address2: "PA 19140, United States",
   },
   {
-    name: "Gipsz Jakab",
-    restaurant: "Restaurant",
-    address: "sadikghsdfghp",
+    name: "Rita Lehigh",
+    restaurant: "PIZZA RANCH",
+    address: "1327 Main Str. Ste 6, Billings Heights",
+    address2: "MT 59105, United States",
   },
 ];
 
@@ -26,6 +29,7 @@ function App() {
             name={item.name}
             restaurant={item.restaurant}
             address={item.address}
+            address2={item.address2}
             key={i}
           />
         );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,11 @@
 const Card = (props) => {
-  const { name, restaurant, address } = props;
+  const { name, restaurant, address, address2 } = props;
   return (
     <div className="card-container">
       <h2>{name}</h2>
       <h6>{restaurant}</h6>
       <p>{address}</p>
+      <p>{address2}</p>
     </div>
   );
 };
